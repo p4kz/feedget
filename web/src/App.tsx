609b7377c54c0ae15e9168349df71843d3133594
p4kz <wolpak@hotmail.com>
2022-05-02 11:37:56 -0300

    add tailwindcss / test

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,21 @@
 
+interface ButtonProps {
+  text?: string
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-400 p-2 rounded hover:bg-violet-700 text-violet-50 transition-colors">{props.text ?? 'hello react' }</button>
+}
+
 function App() {
 
-  return <h1>Hello React</h1>
+  return (
+    <div className="flex gap-2">
+      <Button text='Olá'/>
+      <Button text='tailwind'/>
+      <Button />
+    </div>
+  )
 }
 
 export default App

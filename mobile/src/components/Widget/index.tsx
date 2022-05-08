0fcfabff12aa37b1a3961a28878c4106ba5453be
p4kz <wolpak@hotmail.com>
2022-05-08 13:54:54 -0300

    create success screen

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -5,6 +5,7 @@ import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 import { Options } from '../Options';
+import { Success} from '../Success';
 import { Form } from '../Form';
 
 
@@ -41,8 +42,7 @@ function Widget() {
         handleIndicatorStyle={styles.indicator}
       >
         
-        <Form 
-          feedbackType='BUG'
+        <Success
         />
       </BottomSheet>
     </>
